b7f7418f3a4ea238e22c8aeaa8edb52f0e638962
Sergey Samoylov <sam0lov@yandex.ru>
2026-01-22 22:31:29 +0300

    Implemented shopping-store service

---
+++ commerce/shopping-store/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA IF NOT EXISTS shopping_store;
+
+CREATE TABLE IF NOT EXISTS shopping_store.products (
+    product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    image_src VARCHAR(500),
+    quantity_state VARCHAR(20) NOT NULL CHECK (quantity_state IN ('ENDED', 'FEW', 'ENOUGH', 'MANY')),
+    product_state VARCHAR(20) NOT NULL DEFAULT 'ACTIVE' CHECK (product_state IN ('ACTIVE', 'DEACTIVATE')),
+    product_category VARCHAR(20) NOT NULL CHECK (product_category IN ('LIGHTING', 'CONTROL', 'SENSORS')),
+    price NUMERIC(10, 2) NOT NULL CHECK (price >= 1.00),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    version BIGINT DEFAULT 0
+);
