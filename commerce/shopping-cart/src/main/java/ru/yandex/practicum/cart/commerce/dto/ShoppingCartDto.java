75b241599ebb44b304f3085023b253ae95bcbf0c
Sergey Samoylov <sam0lov@yandex.ru>
2026-01-27 21:16:22 +0300

    Refactor controllers

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/cart/commerce/dto/ShoppingCartDto.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/cart/commerce/dto/ShoppingCartDto.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.cart.commerce.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +12,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ShoppingCartDto {
 
-    @JsonProperty("shoppingCartId")
     private UUID shoppingCartId;
 
-    @JsonProperty("products")
     private Map<UUID, Long> products;
 }
--- commerce/shopping-store/src/main/java/ru/yandex/practicum/store/commerce/controller/ShoppingStoreController.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/store/commerce/controller/ShoppingStoreController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.store.commerce.dto.PageResponseDto;
 import ru.yandex.practicum.store.commerce.dto.ProductDto;
@@ -17,6 +18,7 @@ import ru.yandex.practicum.store.commerce.service.ShoppingStoreService;
 import java.util.UUID;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/api/v1/shopping-store")
 @RequiredArgsConstructor
