cb1494822e0904097507e810bdf4016549f4d3b8
Sergey Samoylov <sam0lov@yandex.ru>
2026-01-29 21:30:09 +0300

    Refactor WarehouseController

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/commerce/controller/WarehouseController.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/commerce/controller/WarehouseController.java
@@ -2,11 +2,13 @@ package ru.yandex.practicum.warehouse.commerce.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.warehouse.commerce.dto.*;
 import ru.yandex.practicum.warehouse.commerce.service.WarehouseService;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/warehouse")
 @RequiredArgsConstructor
@@ -17,27 +19,35 @@ public class WarehouseController {
     @PutMapping
     public ResponseEntity<Void> newProductInWarehouse(
             @Valid @RequestBody NewProductInWarehouseRequest request) {
+        log.info("PUT /api/v1/warehouse");
         warehouseService.addNewProduct(request);
+        log.info("New product: {} added to warehouse", request.getProductId());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/add")
     public ResponseEntity<Void> addProductToWarehouse(
             @Valid @RequestBody AddProductToWarehouseRequest request) {
+        log.info("POST /api/v1/warehouse/add");
         warehouseService.addProductQuantity(request);
+        log.info("Product: {} added to warehouse in quantity of: {}", request.getProductId(), request.getQuantity());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/check")
     public ResponseEntity<BookedProductsDto> checkProductQuantityEnoughForShoppingCart(
             @Valid @RequestBody ShoppingCartDto shoppingCart) {
+        log.info("POST /api/v1/warehouse/check");
         BookedProductsDto result = warehouseService.checkAndReserveProducts(shoppingCart);
+        log.info("Products booked result: {} for shopping cart: {}", result, shoppingCart.getShoppingCartId());
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/address")
     public ResponseEntity<AddressDto> getWarehouseAddress() {
+        log.info("GET /api/v1/warehouse/address");
         AddressDto address = warehouseService.getWarehouseAddress();
+        log.info("Recieved address: {} for delivery", address);
         return ResponseEntity.ok(address);
     }
 }
--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/commerce/dto/DimensionDto.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/commerce/dto/DimensionDto.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.warehouse.commerce.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DimensionDto {
+
+    @NotNull
+    @Min(value = 1)
     private Double width;
+
+    @NotNull
+    @Min(value = 1)
     private Double height;
+
+    @NotNull
+    @Min(value = 1)
     private Double depth;
 }
