9d00da254627cb445f19faeb5a05216ae6e7f625
Sergey Samoylov <sam0lov@yandex.ru>
2026-01-29 20:55:50 +0300

    Fix schemas and pom for start services

--- commerce/shopping-cart/src/main/resources/schema.sql
+++ commerce/shopping-cart/src/main/resources/schema.sql
@@ -1,12 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS shopping_cart;
 
-CREATE TABLE IF NOT EXISTS shopping_cart (
+CREATE TABLE IF NOT EXISTS shopping_cart.shopping_cart (
     shopping_cart_id UUID PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT true
 );
 
-CREATE TABLE IF NOT EXISTS cart_items (
+CREATE TABLE IF NOT EXISTS shopping_cart.cart_items (
     shopping_cart_id UUID NOT NULL,
     product_id UUID NOT NULL,
     quantity BIGINT NOT NULL,
